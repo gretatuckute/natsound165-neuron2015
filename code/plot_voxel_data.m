d287e725a07927d97aad6951e9990024a0df7d1d
Greta Tuckute <gretatu@mit.edu>
2021-03-04 14:58:13 -0500

    test plot vox

--- code/plot_voxel_data.m
+++ code/plot_voxel_data.m
@@ -4,8 +4,18 @@ repo_directory = fileparts(fileparts(which('plot_voxel_data.m')));
 
 % load voxel responses from all subjects stored as a set of 2D grids
 % shows responses smoothed with a 3mm FWHM kernel
-load([repo_directory '/data/voxel_responses_3mm.mat'], 'G');
+load([repo_directory '/data/all_voxel_responses_3mm.mat'], 'G'); % changed to prefix "all_"
 
+% test
+x=load([repo_directory '/data/all_voxel_responses_3mm.mat']);
+y=load([repo_directory '/data/all_voxel_responses_5mm.mat']) % same
+
+%% test data_matrix
+data_matrix=load('data_matrix.mat')
+
+
+
+%%
 % plot map for one subject, sound, repetition and hemisphere
 subj = 1;
 sound = 1;
@@ -25,7 +35,7 @@ addpath([repo_directory '/code/fs'])
 X = G;
 for hemi = 1:2
     X.grid_data{hemi} = squeeze(X.grid_data{hemi}(:,:,subj,:,rep));
-    endl
+end
 
 % interpolate to surface
 S = grid2surface(X);
